2e20bc519519013d6003cd6e8ea32369914bb431
Bas van Marwijk <bascoder@users.noreply.github.com>
2017-05-28 16:03:38 +0200

    Initial changes to default ASP.NET MVC template

---
+++ PetStoreMvc/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PetStoreMvc.MvcApplication" Language="C#" %>
---
+++ PetStoreMvc/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PetStoreMvc.Startup))]
+namespace PetStoreMvc
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
