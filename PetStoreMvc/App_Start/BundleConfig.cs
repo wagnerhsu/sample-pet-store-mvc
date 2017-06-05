14b4fa921af127a3c92fec9cb906a676286b8965
Bas van Marwijk <basvmar@hotmail.com>
2017-06-05 12:04:56 +0200

    Have same locale validation on server side and client side
    * Added jQuery.Validation.Localization
    * Updated jQuery.Validation to v1.16.0
    * Change methods_nl.js to add number format
    * In BundleConfig add jQuery.Validation.Localization with server locale

--- PetStoreMvc/App_Start/BundleConfig.cs
+++ PetStoreMvc/App_Start/BundleConfig.cs
@@ -1,4 +1,5 @@
-﻿using System.Web;
+﻿using System.Threading;
+using System.Web;
 using System.Web.Optimization;
 
 namespace PetStoreMvc
@@ -11,8 +12,11 @@ namespace PetStoreMvc
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
+            var locale = Thread.CurrentThread.CurrentCulture;
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
+                        "~/Scripts/jquery.validate*",
+                        $"~/Scripts/localization/methods_{locale.TwoLetterISOLanguageName.ToLower()}.js",
+                        $"~/Scripts/localization/messages_{locale.TwoLetterISOLanguageName.ToLower()}.js"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
